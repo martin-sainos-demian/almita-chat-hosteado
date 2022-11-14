c748ecb3beefb7cede019e78fa1e4b15de11004d
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-13 19:51:25 -0600

    IP

--- public/js/logica.js
+++ public/js/logica.js
@@ -4,7 +4,7 @@ let salida = document.getElementById("salida")
 let notificaciones = document.getElementById("notificaciones")
 let boton = document.getElementById("enviar")
 //Crear una instancia de SocketIO, recibe como parámetro el url del servidor al que se conectará
-var socket = io.connect('http://192.168.20.80:'+process.env.PORT);
+var socket = io.connect('https://chat-almita.cyclic.app');
 
 var clientes=[]
 
