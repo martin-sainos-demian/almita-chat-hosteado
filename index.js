6fd2f95692e1aeb234568aad67fc6fdc45950be8
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-13 22:06:27 -0600

    ls

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const PORT = process.env.PORT;
 
 //global
 app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://martin-sainos-demian.github.io");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://martin-sainos-demian.github.io")
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
